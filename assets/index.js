d825d760ef5843616cabf62f36507301a19f1e87
niicodeer <nicoradin.job@gmail.com>
2023-05-14 01:54:35 -0300

    Agrego menu para celular e interactividad

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,18 @@
-let menu = document.getElementById('menu__celular');
-let nav = document.getElementById("open");
-nav.addEventListener('click', e => {
-  menu.classList.toggle('mostrar');
+let menu = document.getElementById("menu__celular");
+let nav = document.getElementById("menu__icono");
+let hamburguesa = document.getElementById("menu-hamburguesa");
+let cruz = document.getElementById("menu-cruz");
+let item = document.querySelectorAll(".menu_li");
+
+nav.addEventListener("click", (e) => {
+  menu.classList.toggle("mostrar");
+  hamburguesa.classList.toggle("close");
+  cruz.classList.toggle("close");
+});
+item.forEach((i) => {
+  i.addEventListener("click", (e) => {
+  menu.classList.toggle("mostrar");
+  hamburguesa.classList.toggle('close');
+  cruz.classList.toggle('close'); 
+  });
 });
