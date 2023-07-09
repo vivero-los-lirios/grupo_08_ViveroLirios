9ad2cc4c9c84148f2d3762b494822b92dd170a53
Nico Radin <nicoradin.job@gmail.com>
2023-07-09 14:17:07 -0300

    deleted db password app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -12,7 +12,7 @@ CORS(app) #modulo cors es para que me permita acceder desde el frontend al backe
 
 # configuro la base de datos, con el nombre el usuario y la clave
 #app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://eduz14:MyNewPass@eduz14.mysql.pythonanywhere-services.com/eduz14$losliriosBD'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Nico.BBdd96@localhost/liriosdb'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:password@localhost/liriosDB'
 
 # URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
